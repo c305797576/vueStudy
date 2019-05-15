c581ab1850ce0a2b3670044407af137f385dce42
cai <1090173817@qq.com>
2019-05-15 17:06:40 +0800

    day7

---
+++ vue-book/src/base/Tab.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <router-link to="/home">
+        <i class="iconfont icon-home"></i>
+        <span>首页</span>
+      </router-link>
+      <router-link to="/list">
+        <i class="iconfont icon-liebiao"></i>
+        <span>列表</span>
+      </router-link>
+      <router-link to="/collect">
+        <i class="iconfont icon-collection"></i>
+        <span>收藏</span>
+      </router-link>
+      <router-link to="/add">
+        <i class="iconfont icon-icon-test"></i>
+        <span>添加</span>
+      </router-link>
+    </div>
+</template>
+
+<style scoped lang="less">
+
+</style>
--- vue-book/src/router/index.js
+++ vue-book/src/router/index.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-Vue.use(Router)
+Vue.use(Router);
+import Home from '../components/Home.vue';
+import List from '../components/List.vue';
+import Detail from '../components/Detail.vue';
+import Collect from '../components/Collect.vue';
+import Add from '../components/Add.vue';
 
 export default new Router({
   routes: [
-
+    {path:'/',component:Home},
+    {path:'/home',component:Home},
+    {path:'/list',component:List},
+    {path:'/detail',component:Detail},
+    {path:'/collect',component:Collect},
+    {path:'/add',component:Add},
+    {path:'*',component:Home},
   ]
 })
